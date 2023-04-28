51a8bbcb87cda94029374224f7e55b141c303387
Bert Coates <c3050110@newcastle.ac.uk>
2023-04-28 09:54:03 +0100

    Updated DateConveter.java to protect against accidental identification

--- src/DateConverter.java
+++ src/DateConverter.java
@@ -7,30 +7,42 @@ public class DateConverter {
      * */
 
     public static int getMonthNumber(String month) {
-        switch (month.toLowerCase().strip().substring(0,3)) {
+        switch (month.toLowerCase().strip()) {
             case "jan":
+            case "january":
                 return 1;
             case "feb":
+            case "february":
                 return 2;
             case "mar":
+            case "march":
                 return 3;
             case "apr":
+            case "april":
                 return 4;
             case "may":
                 return 5;
             case "jun":
+            case "june":
                 return 6;
             case "jul":
+            case "july":
                 return 7;
             case "aug":
+            case "august"
                 return 8;
             case "sep":
+            case "sept":
+            case "september":
                 return 9;
             case "oct":
+            case "october":
                 return 10;
             case "nov":
+            case "november":
                 return 11;
             case "dec":
+            case "december":
                 return 12;
             default:
                 return 0;
